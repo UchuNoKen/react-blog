a1755c952eba2f8ba9558f3f27846417068f738a
UchuNoKen <xen.o.fite@gmail.com>
2019-02-11 16:04:41 -0500

    Testing async request.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
 export const fetchPosts = () => {
+  const promise = jsonPlaceholder.get("/posts");
+
   return {
-    type: "FETCH_POSTS"
+    type: "FETCH_POSTS",
+    payload: promise
   };
 };
