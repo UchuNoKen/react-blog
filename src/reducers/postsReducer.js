b133cada7fbc7332765beaa32936fa0e0b0dabbc
UchuNoKen <xen.o.fite@gmail.com>
2019-02-26 12:02:46 -0500

    refactor postsReducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,14 @@
-export default () => {
-  return 123;
+export default (state = [], action) => {
+  // if(action.type === 'FETCH_POSTS'){
+  //     return action.payload;
+  // }
+
+  // return state;
+
+  switch (action.type) {
+    case "FETCH_POSTS":
+      return action.payload;
+    default:
+      return state;
+  }
 };
